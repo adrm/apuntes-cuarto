abd4b72b0929115fa4bfdb9d7001d3e422a00c40
Adrián de la Rosa Martín <adriantovick@gmail.com>
2015-11-03 18:28:36 +0100

    ADBD: Ejercicio de queries SQL.

---
+++ adbd/ejercicio/respuesta.sql
@@ -0,0 +1,28 @@
+-- Listar todas las personas (nombre, f_alta) ordenadas por nombre.
+
+
+-- Devolver el número de amigos de cada persona.
+
+
+-- Seleccionar a la(s) persona(s) con el mayor número de amigos.
+
+
+-- Obtener los nombres de las personas que no han realizado ninguna entrada de estado.
+
+
+-- Para cada persona que haya actualizado al menos dos veces el estado, determinar el tiempo en que han ocurrido esas actualizaciones.
+
+
+-- Encontrar el grupo con mayor número de miembros.
+
+
+-- Contar el número de amigos de 'nivel dos' (amigos de amigos) de la persona 'X'. No contar a la misma persona dos veces.
+
+
+-- Encontrar las 5 personas con mayor número de amigos.
+
+
+-- Obtener el ratio de cohesión de cada grupo (número de enlaces entre miembros / número máximo de enlaces posible entre miembros)
+
+
+-- Encontrar a las dos personas con fecha de nacimiento más cercana, suponiendo que no hay personas con la misma fecha de nacimiento.
